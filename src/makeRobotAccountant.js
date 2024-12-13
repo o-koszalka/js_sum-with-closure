a57abe19d267fc3530fef04410088b9bd2ce2b57
Oskar Koszalka <oskar.koszalka@wp.pl>
2024-12-13 17:49:46 +0100

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counting = 0;
+
+  return (a) => {
+    return (b) => {
+      counting++;
+
+      if (counting <= 3 || counting % 2 !== 0) {
+        return a + b;
+      }
+
+      return 'Bzzz... Error!';
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
